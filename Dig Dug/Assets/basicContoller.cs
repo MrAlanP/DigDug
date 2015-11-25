616c51f2ba28326fa76c1424235b97d54e635f52
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2015-11-25 18:31:24 +0000

    player sprite, basic controls and animations

---
+++ Dig Dug/Assets/basicContoller.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicContoller : MonoBehaviour
+{
+    float speed = 250;
+    Animator ani;
+	// Use this for initialization
+	void Start () 
+    {
+        ani = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+          
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2((Input.GetAxis("Horizontal") * speed) , 0));
+                ani.Play("walkR");
+            }
+            else
+            {
+                ani.Play("walkL");
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
+            }
+        }
+        else if (Input.GetAxis("Vertical")!=0)
+        {
+        
+            if (Input.GetAxis("Vertical")>0)
+            {
+                ani.Play("WalkUp");
+                gameObject.GetComponent<Rigidbody2D>().AddForce( new Vector2(0, Input.GetAxis("Vertical") * speed));
+            }
+            else
+            {
+                ani.Play("walkDown");
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed));
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            ani.Play("playerIdle");
+        }
+	}
+}
