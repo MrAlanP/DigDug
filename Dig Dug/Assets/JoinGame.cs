97229bf26a4b601e03d04ccd147abe2f12cb0538
Liam-Young <Liam2.Young@live.uwe.ac.uk>
2015-11-27 17:18:22 +0000

    Input Mapping
    Always needs to enter before exit

--- Dig Dug/Assets/JoinGame.cs
+++ Dig Dug/Assets/JoinGame.cs
@@ -6,6 +6,12 @@ public class JoinGame : MonoBehaviour {
 
 	public GameObject ReadyTextPlayer1;
 	public GameObject ReadyTextPlayer2;
+	public GameObject ReadyTextPlayer3;
+	public GameObject ReadyTextPlayer4;
+	public GameObject ReadyTextPlayer5;
+	public GameObject ReadyTextPlayer6;
+	public GameObject ReadyTextPlayer7;
+	public GameObject ReadyTextPlayer8;
 	public List<int> PlayerList = new List<int>();
 
 	// Use this for initialization
@@ -20,8 +26,14 @@ public class JoinGame : MonoBehaviour {
     {
 		bool PCReadyP1 = Input.GetKeyDown (KeyCode.Return);
 		bool PCReadyP2 = Input.GetKeyDown (KeyCode.KeypadEnter);
-		bool JoypadReadyP1 = Input.GetKeyDown (KeyCode.JoystickButton4);
-		bool JoypadReadyP2 = Input.GetKeyDown (KeyCode.JoystickButton5);
+		bool JoypadReadyP1 = Input.GetKeyDown (KeyCode.Joystick1Button4);
+		bool JoypadReadyP2 = Input.GetKeyDown (KeyCode.Joystick1Button5);
+		bool JoypadReadyP3 = Input.GetKeyDown (KeyCode.Joystick2Button4);
+		bool JoypadReadyP4 = Input.GetKeyDown (KeyCode.Joystick2Button5);
+		bool JoypadReadyP5 = Input.GetKeyDown (KeyCode.Joystick3Button4);
+		bool JoypadReadyP6 = Input.GetKeyDown (KeyCode.Joystick3Button5);
+		bool JoypadReadyP7 = Input.GetKeyDown (KeyCode.Joystick4Button4);
+		bool JoypadReadyP8 = Input.GetKeyDown (KeyCode.Joystick4Button5);
 
 		if (PCReadyP1) {
 			ReadyTextPlayer1.SetActive (true);
