b71e40113522bdc05421c86397d0fbb62cc50800
Liam-Young <Liam2.Young@live.uwe.ac.uk>
2015-12-01 16:27:47 +0000

    UI
    Referencing makes it sticky

--- Dig Dug/Assets/Scripts/GameUI.cs
+++ Dig Dug/Assets/Scripts/GameUI.cs
@@ -6,78 +6,80 @@ using System.Collections.Generic;
 public class GameUI : MonoBehaviour {
 
 	public JoinGame Joining;
-	public Image Player1;
-	public Image Player2;
-	public Image Player3;
-	public Image Player4;
-	public Image Player5;
-	public Image Player6;
-	public Image Player7;
-	public Image Player8;
+	public GameObject UI;
+	public GameObject Player1;
+	public GameObject Player2;
+	public GameObject Player3;
+	public GameObject Player4;
+	public GameObject Player5;
+	public GameObject Player6;
+	public GameObject Player7;
+	public GameObject Player8;
 	List<Player> players = new List<Player>();
 
 	// Use this for initialization
 	void Start () {
-	
+		UI.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-/*		if (Joining.UpdateCountdown = 0) {
+		if (Joining.UpdateCountdown() = 0) {
 
 			if (Joining.GetPlayers() = 1) {
-				Player1.IsActive (true);
+				Player1.SetActive(true);
 			}
 			else if(Joining.GetPlayers = 2){
-				Player1.IsActive (true);
-				Player2.IsActive (true);
+				Player1.SetActive (true);
+				Player2.SetActive (true);
 			}
 			else if(Joining.GetPlayers = 3){
-				Player1.IsActive (true);
-				Player2.IsActive (true);
-				Player3.IsActive (true);
+				Player1.SetActive (true);
+				Player2.SetActive (true);
+				Player3.SetActive (true);
 			}
 			else if(Joining.GetPlayers = 4){
-				Player1.IsActive (true);
-				Player2.IsActive (true);
-				Player3.IsActive (true);
-				Player4.IsActive (true);
+				Player1.SetActive (true);
+				Player2.SetActive (true);
+				Player3.SetActive (true);
+				Player4.SetActive (true);
 			}
 			else if(Joining.GetPlayers = 5){
-				Player1.IsActive (true);
-				Player2.IsActive (true);
-				Player3.IsActive (true);
-				Player4.IsActive (true);
-				Player5.IsActive (true);
+				Player1.SetActive (true);
+				Player2.SetActive (true);
+				Player3.SetActive (true);
+				Player4.SetActive (true);
+				Player5.SetActive (true);
 			}
 			else if(Joining.GetPlayers = 6){
-				Player1.SetActive ();
-				Player2.SetActive ();
-				Player3.SetActive ();
-				Player4.SetActive ();
-				Player5.SetActive ();
-				Player6.SetActive ();
+				Player1.SetActive (true);
+				Player2.SetActive (true);
+				Player3.SetActive (true);
+				Player4.SetActive (true);
+				Player5.SetActive (true);
+				Player6.SetActive (true);
 			}
 			else if(Joining.GetPlayers = 7){
-				Player1.SetActive ();
-				Player2.SetActive ();
-				Player3.SetActive ();
-				Player4.SetActive ();
-				Player5.SetActive ();
-				Player6.SetActive ();
-				Player7.SetActive ();
+				Player1.SetActive (true);
+				Player2.SetActive (true);
+				Player3.SetActive (true);
+				Player4.SetActive (true);
+				Player5.SetActive (true);
+				Player6.SetActive (true);
+				Player7.SetActive (true);
 			}
 			else if(Joining.GetPlayers = 8){
-				Player1.SetActive ();
-				Player2.SetActive ();
-				Player3.SetActive ();
-				Player4.SetActive ();
-				Player5.SetActive ();
-				Player6.SetActive ();
-				Player7.SetActive ();
-				Player8.SetActive ();
+				Player1.SetActive (true);
+				Player2.SetActive (true);
+				Player3.SetActive (true);
+				Player4.SetActive (true);
+				Player5.SetActive (true);
+				Player5.SetActive (true);
+				Player6.SetActive (true);
+				Player7.SetActive (true);
+				Player8.SetActive (true);
 			}
-		}*/
+		}
 	}
 }
